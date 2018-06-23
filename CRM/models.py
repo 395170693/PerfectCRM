16a1383f128b662ead3732a4b5643e3e115052b3
395170693 <395170693@qq.com>
2018-06-23 12:53:23 +0800

    20180624

--- CRM/models.py
+++ CRM/models.py
@@ -1,15 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    '''客户跟进表'''
+    pass
+
+class CustomerFollowUp(models.Model):
+    '''客户跟进表'''
+    pass
+
 class Course(models.Model):
     '''课程表'''
     pass
+
 class ClassList(models.Model):
     '''班级表'''
     pass
+
+class CourseRecord(models.Model):
+    '''上课记录表'''
+    pass
+
+class Enrollment(models.Model):
+    '''报名表'''
+    pass
+
 class UserProfile(models.Model):
     '''账号表'''
     pass
+
 class Role(models.Model):
     '''角色表'''
     pass
