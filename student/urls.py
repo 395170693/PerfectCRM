e674c541fa0508cff3fc7d8926affd3b5c7b8e3d
395170693@qq.com <395170693@qq.com>
2018-07-06 18:03:45 +0800

    20180703

---
+++ student/urls.py
@@ -0,0 +1,11 @@
+
+from django.conf.urls import url
+from django.conf.urls import include
+from django.contrib import admin
+from student import views
+
+
+
+urlpatterns = [
+    url(r'^$', views.index,name='student_index'),
+]
