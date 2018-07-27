8c000b0bf642e255006fd416a749c6b351b2ac79
395170693@qq.com <395170693@qq.com>
2018-07-27 18:02:08 +0800

    20180703

--- king_admin/king_admin.py
+++ king_admin/king_admin.py
@@ -1,4 +1,7 @@
 
+
+
+from CRM import models
 enabled_admins = {}
 class BaseAdmin(object):
     list_display = []
@@ -8,6 +11,13 @@ class BaseAdmin(object):
 class CustomerAdmin(BaseAdmin):
     list_display = ['qq','name']
 
+class CustomerFollowUpAdmin(BaseAdmin):
+    list_display = ('customer','consultant','date')
+
+
+
+
+
 
 def register(model_class,admin_class=None):
     if model_class._meta.app_label not in enabled_admins:
@@ -16,3 +26,5 @@ def register(model_class,admin_class=None):
     admin_class.model = model_class
     enabled_admins[model_class._meta.app_label][model_class._meta.model_name] = admin_class
 
+register(models.Customer,CustomerAdmin)
+# register(models.CustomerFollowUp,CustomerFollowUpAdmin)
--- king_admin/views.py
+++ king_admin/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from king_admin import king_admin
 
 def index(request):
-    return render(request,'king_admin/table_index.html')
+    return render(request,'king_admin/table_index.html',{'table_list':king_admin.enabled_admins})
