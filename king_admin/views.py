3f11123e61826dfa211560ce5f8ced6966d993ba
395170693@qq.com <395170693@qq.com>
2018-08-13 18:04:14 +0800

    20180703

--- king_admin/views.py
+++ king_admin/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 import importlib
 
 
@@ -10,7 +11,14 @@ def index(request):
 
 def display_table_objs(request,app_name,table_name):
     print ('-------------->',app_name,table_name)
+    admin_class = king_admin.enabled_admins[app_name][table_name]
     # models_module = importlib.import_module('%s.model'%(app_name))
     # model_obj = getattr(models_module,table_name)
-    admin_class = king_admin.enabled_admins[app_name][table_name]
-    return render(request,'king_admin/table_objs.html',{"admin_class":admin_class})
+    object_list = admin_class.model.objects.all()
+    paginator = Paginator(object_list, 2) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    contacts = paginator.get_page(page)
+    #return render(request, 'list.html', {'contacts': contacts})
+    #admin_class = king_admin.enabled_admins[app_name][table_name]
+    return render(request,'king_admin/table_objs.html',{"query_sets":query_sets})
